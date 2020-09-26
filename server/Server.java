982229ff04558d3dd881a8bf003ed8c3eca8cb57
yen-tingLin <aillvone@gmail.com>
2020-09-26 10:01:31 +0800

    revise name

--- server/Server.java
+++ server/Server.java
@@ -5,7 +5,7 @@ public class Server {
     public static void main(String[] args) {
         
         int port = 9090;
-        ServerConnectionImp.start(port);
+        ServerConnection.start(port);
 
     }
     
