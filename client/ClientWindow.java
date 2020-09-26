3066baca781b36c6f1a3d53756a98620abd11f1b
yen-tingLin <aillvone@gmail.com>
2020-09-26 16:10:18 +0800

    client window by swing ui

---
+++ client/ClientWindow.java
@@ -0,0 +1,84 @@
+package client;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+
+public class ClientWindow {
+
+	private JFrame frame;
+	private JTextField messageField;
+	private static JTextArea textArea = new JTextArea();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());					
+					ClientWindow window = new ClientWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ClientWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 667, 461);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		frame.setTitle("Chat Program\n");
+		
+		textArea.setEnabled(false);
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.SOUTH);
+		
+		messageField = new JTextField();
+		panel.add(messageField);
+		messageField.setColumns(50);
+		
+		JButton btnSend = new JButton("Send");
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel.add(btnSend);			
+		
+		frame.setLocationRelativeTo(null);
+
+	}
+	
+	public static void printToConsole(String message) {
+		textArea.setText(textArea.getText() + message + "\n");
+		
+	}
+
+}
